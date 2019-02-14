8a37ad72c2d6a150af9bd5cf1578ec26f65db8d5
francistse23 <wtse.ucsd@gmail.com>
2019-02-13 23:29:10 -0800

    Updates

---
+++ precache-manifest.9a85b3772b21d4b4ecbbabf5cf6c6f7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5f93b3feb5e4b32a722e",
+    "url": "/static/js/main.5f93b3fe.chunk.js"
+  },
+  {
+    "revision": "115531360c65cdb91f83",
+    "url": "/static/js/1.11553136.chunk.js"
+  },
+  {
+    "revision": "5f93b3feb5e4b32a722e",
+    "url": "/static/css/main.0534e237.chunk.css"
+  },
+  {
+    "revision": "dce8a0c2efc0191027673584e4a10960",
+    "url": "/index.html"
+  }
+];
